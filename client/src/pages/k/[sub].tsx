8178e9fa45570aeab23a6026b1382ceb314e393a
Salih Gumus <sudanmerinosu@gmail.com>
2021-04-30 18:01:43 +0300

    Frontend Image upload is done

--- client/src/pages/k/[sub].tsx
+++ client/src/pages/k/[sub].tsx
@@ -1,13 +1,60 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { ChangeEvent, createRef, Fragment, useEffect, useState } from 'react';
 import useSWR from 'swr';
 import PostCard from '../../components/PostCard';
+import Image from 'next/image';
+import classNames from 'classnames';
 
-export default function Sub() {
+import { Sub } from '../../types';
+
+import { useAuthState } from '../../context/auth';
+import axios from 'axios';
+
+export default function SubPage() {
+    //Local state
+    const [ownSub, setOwnSub] = useState(false);
+    //Global State
+    const { authenticated, user } = useAuthState();
+    //Utils
     const router = useRouter();
+    const fileInputRef = createRef<HTMLInputElement>();
 
     const subName = router.query.sub;
 
-    const { data: sub, error } = useSWR(subName ? `/subs/${subName}` : null);
+    const { data: sub, error, revalidate } = useSWR<Sub>(
+        subName ? `/subs/${subName}` : null
+    );
+
+    useEffect(() => {
+        if (!sub) return;
+        setOwnSub(authenticated && user.username === sub.username);
+    }, [sub]);
+
+    const openFileInput = (type: string) => {
+        if (!ownSub) return;
+        fileInputRef.current.name = type;
+        fileInputRef.current.click();
+    };
+
+    const uploadImage = async (event: ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files[0];
+
+        const formData = new FormData();
+
+        formData.append('file', file);
+        formData.append('type', fileInputRef.current.name);
+
+        try {
+            await axios.post<Sub>(`/subs/${sub.name}/image`, formData, {
+                headers: { 'Content-Type': 'multipart/form-data' },
+            });
+
+            revalidate();
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     if (error) router.push('/');
 
@@ -26,8 +73,76 @@ export default function Sub() {
     }
 
     return (
-        <div className='container flex pt-5 text-gray-100'>
-            {sub && <div className='w-160'>{postsMarkup}</div>}
+        <div>
+            <Head>
+                <title>{sub?.title}</title>
+            </Head>
+
+            {sub && (
+                <Fragment>
+                    <input
+                        type='file'
+                        hidden={true}
+                        ref={fileInputRef}
+                        onChange={uploadImage}
+                    />
+                    {/** Sub info and images*/}
+                    <div>
+                        {/** Banner  image*/}
+                        <div
+                            className={classNames('bg-pink-500', {
+                                'cursor-pointer': ownSub,
+                            })}
+                            onClick={() => openFileInput('banner')}
+                        >
+                            {sub.bannerUrl ? (
+                                <div
+                                    className='h-56 bg-pink-500'
+                                    style={{
+                                        backgroundImage: `url(${sub.bannerUrl})`,
+                                        backgroundRepeat: 'no-repeat',
+                                        backgroundSize: 'cover',
+                                        backgroundPosition: 'center',
+                                    }}
+                                ></div>
+                            ) : (
+                                <div className='h-20 bg-pink-500'></div>
+                            )}
+                        </div>
+                        {/**Sub meta data*/}
+                        <div className='h-20 bg-gray-600'>
+                            <div className='container relative flex'>
+                                <div className='absolute' style={{ top: -15 }}>
+                                    <Image
+                                        src={sub.imageUrl}
+                                        alt='Sub'
+                                        className={classNames('rounded-full', {
+                                            'cursor-pointer': ownSub,
+                                        })}
+                                        onClick={() => openFileInput('image')}
+                                        width={70}
+                                        height={70}
+                                    />
+                                </div>
+                                <div className='pt-1 pl-24'>
+                                    <div className='flex items-center'>
+                                        <h1 className='mb-1 text-3xl font-bold text-gray-100'>
+                                            {sub.title}
+                                        </h1>
+                                    </div>
+                                    <p className='text-sm font-bold text-gray-300'>
+                                        /k/{sub.name}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/** Posts & sidebar*/}
+                    <div className='container flex pt-5 text-gray-100'>
+                        <div className='w-160'>{postsMarkup}</div>
+                    </div>
+                </Fragment>
+            )}
         </div>
     );
 }
