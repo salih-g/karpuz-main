05f3cfe0480bc1522f5f5ab97bb68c1601e6eb23
Salih Gumus <sudanmerinosu@gmail.com>
2021-05-03 17:16:44 +0300

    x

--- client/src/components/ActionButton.tsx
+++ client/src/components/ActionButton.tsx
@@ -1,7 +1,7 @@
 export default function ActionButton({ children }) {
-    return (
-        <div className='px-1 py-1.5 mr-1 text-xs text-gray-200 rounded cursor-pointer hover:bg-gray-400'>
-            {children}
-        </div>
-    );
+  return (
+    <div className='px-1 py-1.5 mr-1 text-xs text-gray-200 rounded cursor-pointer hover:bg-gray-400'>
+      {children}
+    </div>
+  );
 }
